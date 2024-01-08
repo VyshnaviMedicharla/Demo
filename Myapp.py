0c6177abfbb44b2a432ffd7f647dc1a4c0baea73
VyshnaviMedicharla <134770359+VyshnaviMedicharla@users.noreply.github.com>
2024-01-08 12:07:41 +0530

    Myapp.py

--- Myapp.py
+++ Myapp.py
@@ -4,7 +4,7 @@ st.header("Amine")
 col1,col2 =st.columns(2)
 with col1:
   st.subheader("Naruto")
-  st.image("./naruto.jpg",caption="Naruto",width=500,height=300use_column_width=True)
+  st.image("./naruto.jpg",caption="Naruto",width=500,height=300,use_column_width=True)
   st.write("Naruto a Nine tailed fox inside")
 with col2:
   st.subheader("Hinata")
