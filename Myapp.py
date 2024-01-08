b2abc41788faf861b5729b55334fe387cb2d5cf2
VyshnaviMedicharla <134770359+VyshnaviMedicharla@users.noreply.github.com>
2024-01-08 16:02:24 +0530

    Myapp.py

--- Myapp.py
+++ Myapp.py
@@ -5,7 +5,7 @@ col1,col2 =st.columns(2)
 with col1:
   st.subheader("Naruto")
   st.image("./naruto.jpg",caption="Naruto",width=300,use_column_width=True)
-  st.write("Naruto is a Hokage")
+  st.write("Naruto is a Hokage of leaf village")
 with col2:
   st.subheader("Hinata")
   st.image("./hinata.jpg",caption="Hinata",width=300,use_column_width= True)
