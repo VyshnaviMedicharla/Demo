856452358046d291deec9dd0dae02fe6b360b594
VyshnaviMedicharla <134770359+VyshnaviMedicharla@users.noreply.github.com>
2024-01-08 12:03:19 +0530

    Myapp.py

--- Myapp.py
+++ Myapp.py
@@ -1,15 +1,15 @@
 import streamlit as st
-st.set_page_config(page_title="Cats")
-st.header("Types of Cats")
+st.set_page_config(page_title="Naruto")
+st.header("Amine")
 col1,col2 =st.columns(2)
 with col1:
-  st.subheader("persian cat")
-  st.image("./persian.jpg",caption="Persian Cat",width=300,use_column_width=True)
-  st.write("Persian cats are cute")
+  st.subheader("Naruto")
+  st.image("./naruto.jpg",caption="Persian Cat",width=300,use_column_width=True)
+  st.write("Naruto a Nine tailed fox inside")
 with col2:
-  st.subheader("Ragdoll cat")
-  st.image("./ragdoll.jpg",caption="Ragdoll Cat",width=300,use_column_width= True)
-  st.write("Ragdoll cats are proud")
+  st.subheader("Hinata")
+  st.image("./hinata.jpg",caption="Ragdoll Cat",width=300,use_column_width= True)
+  st.write("Hinata a Hyūga clan girl")
   
   
   
